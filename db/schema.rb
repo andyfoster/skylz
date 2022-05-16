c019f656bbd84a2f1096790bb4ed573c2e58641b
Andy Foster <andrewfosteris@gmail.com>
2022-05-16 23:06:56 +1200

    Add domains controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_16_095535) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_105548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "domains", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_domains_on_user_id"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
@@ -38,5 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_16_095535) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "domains", "users"
   add_foreign_key "skills", "users"
 end
