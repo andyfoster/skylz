a7e54cc6325e3c7ce16073394beca6cb10d49e54
Andy Foster <andrewfosteris@gmail.com>
2022-05-25 23:02:20 +1200

    Tidy up the new Skill created with new users to show them around

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :skills
   has_many :domains
 
@@ -11,7 +10,7 @@ class User < ApplicationRecord
 
   def create_domain
     # d = Domain.new
-    d = Domain.create(name: 'Your Skills', user_id: self.id)
+    d = Domain.create(name: 'BJJ', user_id: self.id)
     u = User.find(id)
     u.current_domain = d.id
     u.save!
@@ -19,7 +18,9 @@ class User < ApplicationRecord
 
   def create_example_skill
     new_domain = User.find(id).domains.first
-    s = Skill.create(name: 'Triangle Choke from Closed Guard', user_id: self.id, domain_id: new_domain.id, tags: "basic, choke, triangle")
-    Activity.create(description: 'Goes here', user_id: self.id, skill_id: s.id, tags: "amazing, easy, fun", activity_type: "practice", date: Date.yesterday, rating: 5)
+    s = Skill.create(name: 'Triangle from Closed Guard', notes: "Welcome to Skylz! \rThis is an example of a skill. \rYou can ecit or delete this. And of course, you can change BJJ to whatever 'domain' of skills you want to track!", user_id: self.id, domain_id: new_domain.id, tags: "easy, welcome, task")
+    Activity.create(description: "Did 10x reps on each side with my grappling dummy", user_id: self.id, skill_id: s.id, tags: "solo, fun, home", activity_type: "Solo Drills", date: Date.yesterday, rating: 5)
+    Activity.create(description: "Managed to pull this off in rolls with the new white belt. He just escaped.", user_id: self.id, skill_id: s.id, tags: "success", activity_type: "Rolls", date: Date.yesterday, rating: 2)
+    Activity.create(description: "Saw this move in class for the first time. Almost managed it but forgot the last step", user_id: self.id, skill_id: s.id, tags: "class, first time", activity_type: "Class", date: Date.yesterday, rating: 2)
   end
 end
