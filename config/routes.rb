2327f3c693c1d220d1f2e40fc2df9201542e7c33
Andy Foster <andrewfosteris@gmail.com>
2022-06-08 12:28:16 +1200

    Add a routes for /activities to view all for current domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,27 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :domains
-  # devise_for :users
+
   devise_for :users do
     get "/login" => "devise/sessions#new"
     get "/register" => "devise/registrations#new"
   end
+
   resources :skills do
     resources :activities
   end
 
-  get '/new_multi' => 'skills#new_multi', as: :new_multi
+  get '/activities' => 'activities#index'
 
+  get '/new_multi' => 'skills#new_multi', as: :new_multi
   post '/new_multi' => 'skills#create_multi', as: :create_multi
 
   get '/set_current_domain/:id', to: 'domains#set_current', :as => 'set_domain'
 
-  get '/s/', to: 'skills#tag_search', :as => 'tag_search'
+  # get '/s/', to: 'skills#tag_search', :as => 'tag_search'
   # put '/admin/users/:id/activate' => 'admins#activate_user', :as => 'activate_user'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "skills#index"
 end
