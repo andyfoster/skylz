30614d68f24c25cb51b6f1bd2383ef4016723809
Andy Foster <andrewfosteris@gmail.com>
2022-06-09 14:39:35 +1200

    Validations on Skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,7 +4,10 @@ class Skill < ApplicationRecord
   belongs_to :user
   has_one :domain
   has_many :activities, dependent: :destroy
-  validates :name, presence: true
+
+  validates :name, presence: true, uniqueness: true
+  validates :user_id, presence: true
+  validates :domain_id, presence: true
 
   has_rich_text :notes
 
