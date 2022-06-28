6242b89e07f48fe20de73afe9e3b6c30e863bc13
Andy Foster <andrewfosteris@gmail.com>
2022-06-28 18:25:34 +1200

    Migrate PracticeLists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_024232) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_053833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_024232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_domains_on_user_id"
+  end
+
+  create_table "practice_lists", force: :cascade do |t|
+    t.bigint "domain_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["domain_id"], name: "index_practice_lists_on_domain_id"
+    t.index ["user_id"], name: "index_practice_lists_on_user_id"
   end
 
   create_table "skills", force: :cascade do |t|
