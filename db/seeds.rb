a8455cc2247ecd034002f70d0fb24579e07aa24e
Andy Foster <andrewfosteris@gmail.com>
2022-08-12 15:24:32 +1200

    Comment out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Skill.create(name: 'Ruby',
-             notes: 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.', media: 'https://www.ruby-lang.org/en/', tags: 'programming, language, ruby, ruby on rails', category: 'programming', user_id: 1)
-Skill.create(name: 'Ruby on Rails',
-             notes: 'Ruby on Rails is a server-side web application framework written in Ruby under the MIT License. It is based on the model–view–controller (MVC) architectural pattern, providing default structures for a database, a web service, and web pages.', media: 'https://rubyonrails.org/', tags: 'programming, framework, ruby, ruby on rails', category: 'programming', user_id: 1)
-Skill.create(name: 'Rails',
-             notes: 'Rails is a web application framework written in Ruby under the MIT License. It is based on the model–view–controller (MVC) architectural pattern, providing default structures for a database, a web service, and web pages.', media: 'https://rubyonrails.org/', tags: 'programming, framework, ruby, ruby on rails', category: 'programming', user_id: 1)
-Skill.create(name: 'HTML',
-             notes: 'HyperText Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.', media: 'https://www.w3.org/TR/html5/', tags: 'programming, language, html, html5', category: 'programming', user_id: 1)
-Skill.create(name: 'CSS',
-             notes: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.', media: 'https://www.w3.org/Style/CSS/Overview.en.html', tags: 'programming, language, css, css3', category: 'programming', user_id: 1)
-Skill.create(name: 'JavaScript',
-             notes: 'JavaScript is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed and prototype-based. Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web.', media: 'https://www.javascript.com/', tags: 'programming, language, javascript, jquery, ajax, json', category: 'programming', user_id: 1)
-Skill.create(name: 'jQuery',
-             notes: 'jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. It is free software under the MIT License.', media: 'https://jquery.com/', tags: 'programming, library, javascript, jquery, ajax, json', category: 'programming', user_id: 1)
+# Skill.create(name: 'Ruby',
+#              notes: 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.', media: 'https://www.ruby-lang.org/en/', tags: 'programming, language, ruby, ruby on rails', category: 'programming', user_id: 1)
+# Skill.create(name: 'Ruby on Rails',
+#              notes: 'Ruby on Rails is a server-side web application framework written in Ruby under the MIT License. It is based on the model–view–controller (MVC) architectural pattern, providing default structures for a database, a web service, and web pages.', media: 'https://rubyonrails.org/', tags: 'programming, framework, ruby, ruby on rails', category: 'programming', user_id: 1)
+# Skill.create(name: 'Rails',
+#              notes: 'Rails is a web application framework written in Ruby under the MIT License. It is based on the model–view–controller (MVC) architectural pattern, providing default structures for a database, a web service, and web pages.', media: 'https://rubyonrails.org/', tags: 'programming, framework, ruby, ruby on rails', category: 'programming', user_id: 1)
+# Skill.create(name: 'HTML',
+#              notes: 'HyperText Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.', media: 'https://www.w3.org/TR/html5/', tags: 'programming, language, html, html5', category: 'programming', user_id: 1)
+# Skill.create(name: 'CSS',
+#              notes: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.', media: 'https://www.w3.org/Style/CSS/Overview.en.html', tags: 'programming, language, css, css3', category: 'programming', user_id: 1)
+# Skill.create(name: 'JavaScript',
+#              notes: 'JavaScript is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed and prototype-based. Alongside HTML and CSS, JavaScript is one of the three core technologies of the World Wide Web.', media: 'https://www.javascript.com/', tags: 'programming, language, javascript, jquery, ajax, json', category: 'programming', user_id: 1)
+# Skill.create(name: 'jQuery',
+#              notes: 'jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. It is free software under the MIT License.', media: 'https://jquery.com/', tags: 'programming, library, javascript, jquery, ajax, json', category: 'programming', user_id: 1)
