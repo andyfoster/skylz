8e180fab4eb3f32a4c7e31070195617c808306e4
Andy Foster <andrewfosteris@gmail.com>
2022-08-13 23:15:27 +1200

    Hopefully fix SASS error on Heroku deploys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,12 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  
+  # Fix for Heroku Sass Tailwind error
+  # https://github.com/tailwindlabs/tailwindcss/discussions/6738
+  config.assets.css_compressor = nil
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
