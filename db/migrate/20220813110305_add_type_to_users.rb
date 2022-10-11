18b5dfba94fa18f13ec50f091e58b438ed4dcae3
Andy Foster <andrewfosteris@gmail.com>
2022-10-12 02:53:26 +1300

    Change User type to role

--- db/migrate/20220813110305_add_type_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :type, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -119,7 +119,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_110305) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "current_domain"
-    t.string "type"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
