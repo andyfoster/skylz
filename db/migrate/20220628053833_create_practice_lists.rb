55e806059bccc6e413133df876d5f7f34b864ce4
Andy Foster <andrewfosteris@gmail.com>
2022-10-13 13:41:14 +1300

    Fix spelling mistake. TODO fix error on user destroy

--- db/migrate/20220628053833_create_practice_lists.rb
+++ db/migrate/20220628053833_create_practice_lists.rb
@@ -1,9 +1,9 @@
 class CreatePracticeLists < ActiveRecord::Migration[7.0]
   def change
     create_table :practice_lists do |t|
-      t.references :skillsets, null: false, foreign_key: true
+      t.references :skillset, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-
+      # this isn't being remigrated properly - causes problem with skillset/s on user destroy
       t.timestamps
     end
   end
