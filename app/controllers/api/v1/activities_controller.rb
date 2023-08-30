d129b45eb3e33b1f8afbf2adcedc4ff7496f1c1d
Andy Foster <andrewfosteris@gmail.com>
2023-08-30 17:33:36 +1200

    Refactor activities and skills controllers
    - Retrieve activities and skills specific to the current user
    - Exclude unnecessary attributes from the JSON response

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -2,13 +2,12 @@ module Api
   module V1
     class ActivitiesController < ApplicationController
       before_action :authenticate_user_from_token!
-      
+
       def index
-        activities = Activity.all
-        render json: activities
+        activities = current_user.activities
+        render json: activities.as_json(except: [:user_id, :created_at, :updated_at])
       end
 
-      # add more methods here
     end
   end
 end
--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -1,11 +1,13 @@
 module Api
   module V1
     class SkillsController < ApplicationController
+      before_action :authenticate_user_from_token!
+
       def index
-        skills = Skill.all
-        render json: skills
+        skills = current_user.skills
+        render json: skills.as_json(except: [:user_id, :created_at, :updated_at])
       end
-      
+
     end
   end
 end
