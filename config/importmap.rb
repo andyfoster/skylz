7e98d1ffaf7c93be098c0740ebcedde2f492183d
Andy Foster <andrewfosteris@gmail.com>
2023-09-01 21:18:37 +1200

    Add Flowbite and Popper.js to importmap.rb and update tailwind.config.js
    - Add Flowbite and Popper.js to the importmap.rb file
    - Update the content array in tailwind.config.js to include './node_modules/flowbite/**/*.js'
    - Add 'flowbite/plugin' to the plugins array in tailwind.config.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
+pin "flowbite", to: "https://ga.jspm.io/npm:flowbite@1.8.1/lib/esm/index.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,26 +1,28 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-        header: ['Gemunu Libre'],
-      },
-      colors: {
-        ...defaultTheme.colors,
-        blue: '#1fb6ff',
-      },
+    content: [
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}',
+        './node_modules/flowbite/**/*.js',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                header: ['Gemunu Libre'],
+            },
+            colors: {
+                ...defaultTheme.colors,
+                blue: '#1fb6ff',
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('flowbite/plugin'),
+    ],
 };
