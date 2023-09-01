22f9248836e1bfbd2323b03695b81b8616cd24d9
Andy Foster <andrewfosteris@gmail.com>
2023-09-01 20:16:55 +1200

    Add skip_before_action to SkillsController and implement create action
    - Add `skip_before_action :verify_authenticity_token` to SkillsController
    - Implement `create` action in SkillsController
    - Create a new skill using current_user and skill_params
    - If the skill is saved successfully, render the skill as JSON with status :created
    - If there are errors, render the error messages as JSON with status :unprocessable_entity

--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -1,11 +1,28 @@
 module Api
   module V1
     class SkillsController < ApplicationController
+      skip_before_action :verify_authenticity_token
       before_action :authenticate_user_from_token!
 
       def index
         skills = current_user.skills
         render json: skills.as_json(except: [:user_id, :created_at, :updated_at])
+      end
+
+      def create
+        skill = current_user.skills.build(skill_params)
+        if skill.save
+          render json: skill.as_json(except: [:user_id, :created_at, :updated_at]), status: :created
+        else
+          render json: { errors: skill.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def skill_params
+        params.fetch(:skill, {}).permit(:name, :notes, :media, :tags, :steps, :category,
+                                        :skillset_id, :reason).merge(user_id: current_user.id)
       end
 
     end
