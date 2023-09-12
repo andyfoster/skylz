dfb2ba43163add84189ff93115e99e7094f854fa
Andy Foster <andrewfosteris@gmail.com>
2023-09-12 16:57:37 +1200

    Put in turbo drive = false for navbar. Expect some logout issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,14 @@ import 'controllers';
 import "chartkick"
 import "Chart.bundle"
 import 'flowbite';
+
+// You might need this to make surethe nav drawer always fires
+// but it might be better to just use the data-turbo-permanent attribute??
+// document.addEventListener("turbo:load", function() {
+//   document.querySelector("nav").addEventListener("turbo:click", function() {
+//     document.querySelector("nav").classList.toggle("active");
+//   });
+// });
+
+// The session delete path is not working with this set to false - nav vs log out?
+Turbo.session.drive = false;
