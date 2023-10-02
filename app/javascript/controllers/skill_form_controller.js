1eab8fbf59eb7bd75e3799cf711347b0f33f981a
Andy Foster <andrewfosteris@gmail.com>
2023-10-02 23:41:59 +1300

    Try to fill in skill form with ChatGPT data
    Should really be parsing the json in the backend before sending it to the view

--- app/javascript/controllers/skill_form_controller.js
+++ app/javascript/controllers/skill_form_controller.js
@@ -21,37 +21,147 @@ export default class extends Controller {
     }
 
     generate() {
-
       // pop up an alert with the value of the name field and stay on the page
       // alert(this.skillNameInputTarget.value);
 
-      if (this.skillNameInputTarget.value == "") {
-        alert("Please enter a skill name");
+      if (this.skillNameInputTarget.value == '') {
+        alert('Please enter a skill name');
         return false;
       }
+
+      // Make a GET request to skill/generate and fill in the form with the response
+      fetch('/generate?message=' + this.skillNameInputTarget.value)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          try {
+            console.log(data);
+
+            // Check if the necessary keys exist in the data
+            if (
+              !(
+                'reason' in data &&
+                'steps' in data &&
+                'notes' in data &&
+                'tags' in data
+              )
+            ) {
+              throw new Error('Missing required keys in the response data');
+            }
+
+            this.skillReasonInputTarget.value = data.reason;
+            this.skillStepsInputTarget.value = Array.isArray(data.steps)
+              ? data.steps.join('\n')
+              : '';
+            this.skillNotesInputTarget.value = data.notes;
+            this.skillTagsInputTarget.value = Array.isArray(data.tags)
+              ? data.tags.join(', ')
+              : '';
+
+            console.log('Data successfully parsed and form populated.');
+          } catch (e) {
+            console.error(
+              'An error occurred while parsing or populating data:',
+              e
+            );
+          }
+        })
+        .catch((error) => {
+          console.error('An error occurred while fetching data:', error);
+        });
 
       // stop and don't submit form
       // return false;
 
-
-
-
-    // }
-
-    // generate2() {
+      //   // Simulated API response
+      //   const apiResponse = {
+      //     data: [
+      //       `{
+      //   "reason": "To control and submit an opponent from the spiderweb position",
+      //   "note": "The Americana from Spiderweb is a highly effective submission technique that can be used when you have your opponent immobilized in the spiderweb position. It is a joint lock targeting the opponent's shoulder joint, causing immense pain and potential injury if applied correctly. This skill is commonly used in Brazilian Jiu-Jitsu and submission grappling competitions.",
+      //   "steps": [
+      //     "*set up",
+      //     "Secure your grip on the opponent's left wrist with your right hand",
+      //     "Slide your right knee over the opponent's left arm",
+      //     "Bring your left hand through the gap created by your right knee and grab your own right wrist",
+      //     "Apply downward pressure on the opponent's left wrist with your right hand while simultaneously lifting their elbow with your left hand",
+      //     "Maintain control and gradually increase the pressure to achieve the submission",
+      //     "Note: Be mindful of your partner's safety and tap out early when training",
+      //     "*position",
+      //     "You and your opponent are in the spiderweb position. Your right leg is over their left arm, and your left leg is over their head, trapping them in place.",
+      //     "Next steps..."
+      //   ],
+      //   "tags": ["Brazilian Jiu-Jitsu", "Submission Grappling", "Joint Lock", "Spiderweb Position"]
+      // }`,
+      //     ],
+      //   };
 
       // make a get request to skill/generate and fill in the form with the response
-      fetch('/generate?message=' + this.skillNameInputTarget.value)
+      //   fetch('/generate?message=' + this.skillNameInputTarget.value)
 
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
+      //     .then(response => response.json())
+      //     // need to catch errors here
+      //     .catch(error => console.log(error))
 
-          // this.skillNameInputTarget.value = data.res;
-          this.skillReasonInputTarget.value = data.reason;
-          this.skillStepsInputTarget.value = data.steps.join("\n");
-          this.skillNotesInputTarget.value = data.notes;
-          this.skillTagsInputTarget.value = data.tags.join(", ");
-        });
+      //     .then(data => {
+      //       console.log(data);
+
+      //       // this.skillNameInputTarget.value = data.res;
+      //       this.skillReasonInputTarget.value = data.reason;
+      //       this.skillStepsInputTarget.value = data.steps.join("\n");
+      //       this.skillNotesInputTarget.value = data.notes;
+      //       this.skillTagsInputTarget.value = data.tags.join(", ");
+      //     });
+
+      // try {
+      //   // Access the data array
+      //   const dataArray = apiResponse.data;
+
+      //   // Get the first element, which is a JSON string
+      //   const jsonString = dataArray[0];
+
+      //   // Parse the JSON string into an object
+      //   const parsedData = JSON.parse(jsonString);
+
+      //   // Use the properties of the parsed object to populate your form
+      //   // For example:
+      //   // document.querySelector('#reason').value = parsedData.reason;
+      //   // document.querySelector('#note').value = parsedData.note;
+
+      //   this.skillReasonInputTarget.value = parsedData.reason;
+      //   // this.skillStepsInputTarget.value = data.steps.join("\n");
+      //   this.skillNotesInputTarget.value = parsedData.note;
+
+      //   // Populate steps and tags similarly...
+      //   console.log('Data successfully parsed and form populated.');
+      // } catch (e) {
+      //   // Log the exception and stop further execution
+      //   console.error('An error occurred:', e);
+      // }
+
+      // }
+
+      // generate2() {
+
+      // make a get request to skill/generate and fill in the form with the response
+      //   fetch('/generate?message=' + this.skillNameInputTarget.value)
+
+      //     .then(response => response.json())
+      //     // need to catch errors here
+      //     .catch(error => console.log(error))
+
+      //     .then(data => {
+      //       console.log(data);
+
+      //       // this.skillNameInputTarget.value = data.res;
+      //       this.skillReasonInputTarget.value = data.reason;
+      //       this.skillStepsInputTarget.value = data.steps.join("\n");
+      //       this.skillNotesInputTarget.value = data.notes;
+      //       this.skillTagsInputTarget.value = data.tags.join(", ");
+      //     });
     }
 }
