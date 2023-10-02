cd85ca75371dae3df656d2dd08d290ca14db503a
Andy Foster <andrewfosteris@gmail.com>
2023-10-02 23:36:47 +1300

    Auto format tailwind js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,28 +1,34 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    content: [
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js',
-        './app/views/**/*.{erb,haml,html,slim}',
-        './node_modules/flowbite/**/*.js',
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-                header: ['Gemunu Libre'],
-            },
-            colors: {
-                ...defaultTheme.colors,
-                blue: '#1fb6ff',
-            },
-        },
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './node_modules/flowbite/**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        header: ['Poiret One, sans-serif'],
+        raleway: ['Raleway'],
+        poppins: ['Poppins', 'sans-serif'],
+        delaGothicOne: ['Dela Gothic One'],
+        poiretOne: ['Poiret One'],
+        righteous: ['Righteous'],
+        // header: ['Gemunu Libre'],
+      },
+      colors: {
+        ...defaultTheme.colors,
+        blue: '#1fb6ff',
+      },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/typography'),
-        // require('flowbite/plugin'),
-    ],
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    // require('flowbite/plugin'),
+  ],
 };
