09ae9bc9bb5ae9508231708d78cc30486bbc5b35
Andy Foster <andrewfosteris@gmail.com>
2023-10-02 23:42:55 +1300

    Remove some validations to get the skill sessions to work

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,9 +6,14 @@ class Activity < ApplicationRecord
   belongs_to :skill_session
 
   validates :user_id, presence: true
-  validates :date, presence: true
+  # validates :date, presence: true
   validates :skill_id, presence: true
-  validates :activity_type, presence: true
-  validates :rating, presence: true
+  # validates :activity_type, presence: true
+  # validates :rating, presence: true
   validates :reps, presence: true
+
+
+   def skillset_id
+      skill.skillset_id
+    end
 end
